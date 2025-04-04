1a212009bee5dccca78d0f1e5dec130fef119f6c
Natnael Terefe <154891539+nati1322@users.noreply.github.com>
2025-04-04 18:18:51 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from .models import Post, Comment
 from taggit.forms import TagField
 
+TagWidget() 
+widgets
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
